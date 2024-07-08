3dcb6c4931fc4f3574eac9696508cbb1a110a692
ValeriyaNow <wal.now@yandex.ru>
2024-07-09 00:34:11 +0300

    hw3

--- hw2app/urls.py
+++ hw2app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    
+    path('', views.index, name='index'),
     path('order/', views.facke_data, name='order'),
+    path('client/', views.clients_list, name='client'),
+    path('client/orders/<int:client_id>', views.client_orders, name='client_orders'),
+    path('client/orders/<int:client_id>/day', views.client_orders_day, name='client_orders_day'),
+    path('client/orders/<int:client_id>/mounth', views.client_orders_mounth, name='client_orders_day'),
+    path('client/orders/<int:client_id>/year', views.client_orders_year, name='client_orders_day'),
+
+
       ]
--- hw2app/views.py
+++ hw2app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Client, Product, Order
 import random
 from django.http import HttpResponse
+from django.utils import timezone
+from datetime import timedelta
 
 
 
@@ -26,14 +28,97 @@ def facke_data(request):
 
 
     for i in range(15):
-        order=Order(client=random.choice(clients),total_amount=0)
+        order=Order(client=random.choice(clients),total_amount=0, order_date='2022-06-25')
         num=random.randint(1,9)
         order.save()
         for i in range(num):
             order.products.add(random.choice(products))
         Order.calculate_total_amount(order)
         order.save()
+
+    for i in range(1,47):
+        person=Order.objects.get(id=i)
+        person.delete()
+
     return HttpResponse('order')
+
+def index(request):
+    """Главная страница."""
+    return render(request, 'base.html')
+
+
+def clients_list(request):
+    """Список клиентов."""
+    clients = Client.objects.all()
+    context = {'clients': clients}
+    return render(request, 'client.html', context)
+
+
+def client_orders(request, client_id):
+    client_change = Client.objects.get(pk=client_id)
+    client_orders_all=Order.objects.filter(client=client_change).all()
+    set_order=[]
+    for i in client_orders_all:
+        set_order_item=i.products.all().distinct()
+        for k in set_order_item:
+            set_order.append(k)
+            
+    context = {
+        'client_name': client_change.name,
+        'client_orders_all': set(set_order),
+        'client_id':client_change.id,
+    }
+    return render(request, 'order.html', context)
+
+def client_orders_day(request, client_id):
+    client_change = Client.objects.get(pk=client_id)
+    last_7_days = timezone.now() - timedelta(days=7)
+    client_orders_last_7_days = Order.objects.filter(client=client_change, order_date__gte=last_7_days).all()
+    set_order1=[]
+    for i in client_orders_last_7_days:
+        set_order_item=i.products.all().distinct()
+        for k in set_order_item:
+            set_order1.append(k)
+    
+    
+    context = {
+        'client_name': client_change.name,    
+        'client_orders_last_7_days': set(set_order1),
+        
+    }
+    return render(request, 'order_day.html', context)
+
             
 
-    
+def client_orders_mounth(request, client_id):
+    client_change = Client.objects.get(pk=client_id)
+    last_30_days = timezone.now() - timedelta(days=30)
+    client_orders_last_30_days = Order.objects.filter(client=client_change, order_date__gte=last_30_days).all()
+    set_order2=[]
+    for i in client_orders_last_30_days:
+        set_order_item=i.products.all().distinct()
+        for k in set_order_item:
+            set_order2.append(k)
+    print(set_order2)
+    context = {
+        'client_name': client_change.name,    
+        'client_orders_last_30_days': set(set_order2),
+        
+    }
+    return render(request, 'order_mounth.html', context)   
+
+def client_orders_year(request, client_id):
+    client_change = Client.objects.get(pk=client_id)
+    last_365_days = timezone.now() - timedelta(days=365)
+    client_orders_last_365_days = Order.objects.filter(client=client_change, order_date__gte=last_365_days).all()
+    set_order3=[]
+    for i in client_orders_last_365_days:
+        set_order_item=i.products.all().distinct()
+        for k in set_order_item:
+            set_order3.append(k)
+    context = {
+        'client_name': client_change.name,    
+        'client_orders_last_365_days': set(set_order3),
+        
+    }
+    return render(request, 'order_year.html', context)   
